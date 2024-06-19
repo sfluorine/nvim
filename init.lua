326348ae7b14bbfdee8893b6d6429a6fbedaddfe
sfluorine <diablodvorak@gmail.com>
2024-06-19 23:32:38 +0700

    update config.

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,25 +17,27 @@ vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
 require("lazy").setup("plugins")
 
+vim.notify = require("notify")
+
 vim.cmd([[
     set nocompatible
-	set showmatch
-	set ignorecase
-	set mouse=v
-	set hlsearch
-	set incsearch
-	set tabstop=4
-	set softtabstop=4
-	set expandtab
-	set shiftwidth=4
-	set wildmode=longest,list
-	set cc=80
-	set clipboard=unnamedplus
-	set spell
-	set noswapfile
-	set backupdir=~/.cache/vim
+    set showmatch
+    set ignorecase
+    set mouse=v
+    set hlsearch
+    set incsearch
+    set tabstop=4
+    set softtabstop=4
+    set expandtab
+    set shiftwidth=4
+    set wildmode=longest,list
+    set cc=80
+    set clipboard=unnamedplus
+    set spell
+    set noswapfile
+    set backupdir=~/.cache/vim
 
-	filetype plugin on
+    filetype plugin on
 ]])
 
 local which_key = require("which-key")
