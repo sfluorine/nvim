477c365ddad4636353a65d302f28ea506411834b
sfluorine <diablodvorak@gmail.com>
2024-06-19 17:15:51 +0700

    fix indenting.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,11 +1,11 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function ()
-			local configs = require("nvim-treesitter.configs")
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function ()
+            local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
+            configs.setup({
                 ensure_installed = {
                     "vimdoc",
                     "c",
@@ -19,43 +19,43 @@ return {
                     "markdown",
                 },
                 auto_install = false,
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end
-	},
-	{
-		"slugbyte/lackluster.nvim",
-		lazy = false,
-		priority = 1000,
-		init = function()
-			-- vim.cmd.colorscheme("lackluster")
-			-- vim.cmd.colorscheme("lackluster-hack") -- my favorite
-			vim.cmd.colorscheme("lackluster-mint")
-		end
-	},
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-		init = function()
-			require("mini.animate").setup()
-			require("mini.basics").setup()
-			require("mini.comment").setup()
-			require("mini.completion").setup()
-			require("mini.cursorword").setup()
-			require("mini.files").setup()
-			require("mini.indentscope").setup()
-			require("mini.move").setup()
-			require("mini.notify").setup()
-			require("mini.pairs").setup()
-			require("mini.starter").setup()
-			require("mini.statusline").setup()
-			require("mini.surround").setup()
-			require("mini.trailspace").setup()
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    },
+    {
+        "slugbyte/lackluster.nvim",
+        lazy = false,
+        priority = 1000,
+        init = function()
+            -- vim.cmd.colorscheme("lackluster")
+            -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+            vim.cmd.colorscheme("lackluster-mint")
+        end
+    },
+    {
+        "echasnovski/mini.nvim",
+        version = false,
+        init = function()
+            require("mini.animate").setup()
+            require("mini.basics").setup()
+            require("mini.comment").setup()
+            require("mini.completion").setup()
+            require("mini.cursorword").setup()
+            require("mini.files").setup()
+            require("mini.indentscope").setup()
+            require("mini.move").setup()
+            require("mini.notify").setup()
+            require("mini.pairs").setup()
+            require("mini.starter").setup()
+            require("mini.statusline").setup()
+            require("mini.surround").setup()
+            require("mini.trailspace").setup()
             require("mini.tabline").setup()
-		end
-	},
+        end
+    },
     {
         "smoka7/hop.nvim",
         version = "*",
